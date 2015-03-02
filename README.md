ec647f035651f892eb5dbd4cd3f00425c97656a3
Charles Engelke <git@engelke.com>
2015-03-02 15:44:09 -0500

    Initial working code

---
+++ README.md
@@ -0,0 +1,22 @@
+X.509 Certificate Creation Sample
+=================================
+
+This web page with associated JavaScript allows a user to
+create a self-signed X.509 certificate.
+
+This example is provided to illustrate how to use the W3C
+[Web Cryptography API](http://www.w3.org/TR/WebCryptoAPI/ "API Draft")
+and [PKIjs](https://pkijs.org/ "PKIjs Home Page") libraries.
+
+Using this example requires a web browser that implements a compatible version
+of the Web Cryptography API.
+When the example was created, current versions of Google Chrome,
+Firefox, and Opera browsers could run the example.
+
+**This is not intended to be a production tool.** Rather, it may
+be helpful to developers who intend to create their own tools using
+the Web Cryptography API.
+
+Copyright (c) 2015 Info Tech, Inc.
+Provided under the MIT license.
+See LICENSE file for details.
---
+++ certs.js
@@ -0,0 +1,236 @@
+// X.509 Self-signed Certificate with Web Cryptography API and PKIjs
+//
+// Copyright (c) 2015 Info Tech, Inc.
+// Provided under the MIT license.
+// See LICENSE file for details.
+
+document.addEventListener("DOMContentLoaded", function() {
+    "use strict";
+
+    // Fix Apple prefix if needed
+    if (window.crypto && !window.crypto.subtle && window.crypto.webkitSubtle) {
+        window.crypto.subtle = window.crypto.webkitSubtle;  // Won't work if subtle already exists
+    }
+
+    // Check that web crypto is even available
+    if (!window.crypto || !window.crypto.subtle) {
+        alert("Your browser does not support the Web Cryptography API! This page will not work.");
+        return;
+    }
+
+    // Check that encoding API is also available
+    if (!window.TextEncoder || !window.TextDecoder) {
+        alert("Your browser does not support the Encoding API! This page will not work.");
+        return;
+    }
+
+    document.getElementById("create-certificate").addEventListener("click", createCertificate);
+
+    function createCertificate() {
+        var commonName = document.getElementById("common-name").value;
+        var countryCode = document.getElementById("country-code").value.toUpperCase();
+        var keyPair;
+
+        if (!commonName) {
+            alert("You must enter a name for the certificate.");
+            return;
+        }
+
+        if (countryCode.length !== 2) {
+            alert("Country codes must be two characters long.");
+            return;
+        }
+
+        window.crypto.subtle.generateKey(
+            {
+                name: "RSASSA-PKCS1-v1_5",
+                modulusLength: 2048,
+                publicExponent: new Uint8Array([1, 0, 1]),  // 24 bit representation of 65537
+                hash: {name: "SHA-256"}
+            },
+            true,   // Must extract private key to create PEM files later
+            ["sign", "verify"]).
+        then(function(newKeyPair) {
+            keyPair = newKeyPair;
+            return keyPair;
+        }).
+        then(function(keyPair) {
+            return buildCertificateObject(commonName, countryCode, keyPair);
+        }).
+        then(function(cert) {
+            var pemCert = convertBinaryToPem(cert.toSchema(true).toBER(false), "CERTIFICATE");
+            document.getElementById("pem-certificate").innerText = pemCert;
+        }).
+        then(function() {
+            window.crypto.subtle.exportKey('spki', keyPair.publicKey).
+            then(function(spki) {
+                var pemPublicKey = convertBinaryToPem(spki, "PUBLIC KEY");
+                document.getElementById("pem-public-key").innerText = pemPublicKey;
+            });
+
+            window.crypto.subtle.exportKey('pkcs8', keyPair.privateKey).
+            then(function(pkcs8) {
+                var pemPrivateKey = convertBinaryToPem(pkcs8, "PRIVATE KEY");
+                document.getElementById("pem-private-key").innerText = pemPrivateKey;
+            });
+        }).
+        catch(function(err) {
+            alert("Error generating creating certificate: " + err.message);
+        });
+    }
+
+    // Returns a Promise yielding the certificate object
+    function buildCertificateObject(commonName, countryCode, keyPair) {
+        var cert = new org.pkijs.simpl.CERT();
+
+        setSerialNumber(cert, Date.now());
+        setIssuer(cert, countryCode, null, null, commonName);
+        setValidityPeriod(cert, new Date(), 730);  // Good from today for 730 days
+        setEmptyExtensions(cert);
+        setCABit(cert, false);
+        setKeyUsage(cert, true, true, false, false, false, true, true); // digitalSignature, nonRepudiation, keyCertSign, cRLSign
+        setSignatureAlgorithm(cert, "1.2.840.113549.1.1.11"); // RSA with SHA-256
+
+        return setPublicKey(cert, keyPair.publicKey).
+            then(function() {return signCert(cert, "1.2.840.113549.1.1.11", keyPair.privateKey)}).
+            then(function() {return cert});
+
+
+        // Helper functions
+
+        function setSerialNumber(cert, serialNumber) {
+            cert.serialNumber = new org.pkijs.asn1.INTEGER({value: serialNumber});;
+        }
+
+        function setIssuer(cert, countryCode, organization, orgUnit, commonName) {
+            if (countryCode) {
+                cert.issuer.types_and_values.push(new org.pkijs.simpl.ATTR_TYPE_AND_VALUE({
+                        type: "2.5.4.6", //countryCode
+                        value: new org.pkijs.asn1.PRINTABLESTRING({value: countryCode})
+                }));
+            }
+
+            if (organization) {
+                cert.issuer.types_and_values.push(new org.pkijs.simpl.ATTR_TYPE_AND_VALUE({
+                    type: "2.5.4.8", //organization
+                    value: new org.pkijs.asn1.PRINTABLESTRING({value: organization})
+                }));
+            }
+
+            if (orgUnit) {
+                cert.issuer.types_and_values.push(new org.pkijs.simpl.ATTR_TYPE_AND_VALUE({
+                    type: "2.5.4.7", //orgUnit
+                    value: new org.pkijs.asn1.PRINTABLESTRING({value: orgUnit})
+                }));
+            }
+
+            if (commonName) {
+                cert.issuer.types_and_values.push(new org.pkijs.simpl.ATTR_TYPE_AND_VALUE({
+                    type: "2.5.4.3", //commonName
+                    value: new org.pkijs.asn1.PRINTABLESTRING({value: commonName})
+                }));
+            }
+        }
+
+        function setValidityPeriod(cert, startDate, durationInDays) {
+            // Normalize to midnight
+            var start = new Date(startDate);
+            start.setHours(0);
+            start.setMinutes(0);
+            start.setSeconds(0);
+            var end   = new Date(start.getTime() + durationInDays * 24 * 60 * 60 * 1000);
+
+            cert.notBefore.value = start;
+            cert.notAfter.value  = end;
+        }
+
+        function setEmptyExtensions(cert) {
+            cert.extensions = new Array();
+        }
+
+        function setCABit(cert, isCA) {
+            var basicConstraints = new org.pkijs.simpl.x509.BasicConstraints({
+                cA: isCA,
+                pathLenConstraint: 3
+            });
+
+            cert.extensions.push(new org.pkijs.simpl.EXTENSION({
+                extnID: "2.5.29.19",
+                critical: false,
+                extnValue: basicConstraints.toSchema().toBER(false),
+                parsedValue: basicConstraints
+            }));
+        }
+
+        function setKeyUsage(cert, digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment, keyAgreement, keyCertSign, cRLSign) {
+            var keyUsageBits = new ArrayBuffer(1);
+            var keyUsageBytes = new Uint8Array(keyUsageBits);
+
+            keyUsageBytes[0] = 0;
+            if (digitalSignature)   {keyUsageBytes[0] |= 0x80;}
+            if (nonRepudiation)     {keyUsageBytes[0] |= 0x40;}
+            if (keyEncipherment)    {keyUsageBytes[0] |= 0x20;}
+            if (dataEncipherment)   {keyUsageBytes[0] |= 0x10;}
+            if (keyAgreement)       {keyUsageBytes[0] |= 0x08;}
+            if (keyCertSign)        {keyUsageBytes[0] |= 0x04;}
+            if (cRLSign)            {keyUsageBytes[0] |= 0x02;}
+
+            var keyUsage = new org.pkijs.asn1.BITSTRING({value_hex: keyUsageBits});
+            cert.extensions.push(new org.pkijs.simpl.EXTENSION({
+                extnID: "2.5.29.15",
+                critical: false,
+                extnValue: keyUsage.toBER(false),
+                parsedValue: keyUsage
+            }));
+        }
+
+        function setSignatureAlgorithm(cert, oid) {
+            cert.signatureAlgorithm.algorithm_id = oid; // In tbsCert
+        }
+
+        function setPublicKey(cert, publicKey) {
+            return cert.subjectPublicKeyInfo.importKey(publicKey);
+        }
+
+        function signCert(cert, oid, privateKey) {
+            cert.signature.algorithm_id = oid; // In actual signature
+            return cert.sign(privateKey);
+        }
+    }
+
+
+    // General helper functions
+
+    function arrayBufferToBase64String(arrayBuffer) {
+        var byteArray = new Uint8Array(arrayBuffer)
+        var byteString = '';
+
+        for (var i=0; i<byteArray.byteLength; i++) {
+            byteString += String.fromCharCode(byteArray[i]);
+        }
+
+        return btoa(byteString);
+    }
+
+
+    function convertBinaryToPem(binaryData, label) {
+        var base64Cert = arrayBufferToBase64String(binaryData);
+
+        var pemCert = "-----BEGIN " + label + "-----\r\n";
+
+        var nextIndex = 0;
+        var lineLength;
+        while (nextIndex < base64Cert.length) {
+            if (nextIndex + 63 <= base64Cert.length) {
+                pemCert += base64Cert.substr(nextIndex, 63) + "\r\n";
+            } else {
+                pemCert += base64Cert.substr(nextIndex) + "\r\n";
+            }
+            nextIndex += 63;
+        }
+
+        pemCert += "-----END " + label + "-----\r\n";
+        return pemCert;
+    }
+
+});
